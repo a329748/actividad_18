a94655ae7d601a016b7e6e6da8169e5529690f51
Julián Terán <a329748@uach.mx>
2021-05-14 17:35:01 -0600

    Final

--- script.js
+++ script.js
@@ -2,35 +2,21 @@ const app = Vue.createApp({
 	data() {
 		return {
 			users: [],
-			usersA: []
 		}
 	},
 
 	created() {
 		this.getUsers()
-		this.getUsersA()
 	},
 
 	methods: {
-		getUsers() {
-			fetch('http://jsonplaceholder.typicode.com/users')
-				.then(response => response.json())
-				.then(json => {
-					this.users = json
-				})
-		},
-
-		async getUsersA() {
+		async getUsers() {
 			const url = "http://jsonplaceholder.typicode.com/users"
 			const response = await fetch(url)
 			const users = await response.json()
-			this.usersA = users
+			this.users = users
 		}		
 	},
-
-	computed: {
-
-	}
 })
 
 const vm = app.mount('#app')
